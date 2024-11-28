dceb2b13eead9d7c23f0a833d5ac368909d3d97e
Bharath j r <bharathk9828@gmail.com>
2024-11-28 22:17:02 +0530

    Arithmetic operations.c

---
+++ Arithmetic operations.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+int main()
+
+{
+
+ prinƞ("Enter two operand!\n");
+
+ int a,b;
+
+ scanf("%d%d",&a,&b);
+
+ prinƞ("\nAddition is:%d\n",a+b);
+
+ prinƞ("Subtracion is:%d\n",a-b);
+
+ prinƞ("MulƟplicaion is:%d\n",a*b);
+
+ prinƞ("Division is:%d\n",a/b);
+
+ return 0;}
